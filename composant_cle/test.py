fbf8f60705cb6e37ae09dfa1e37979f120020520
TabbakhMohamedAmine <92047698+TabbakhMohamedAmine@users.noreply.github.com>
2023-05-21 19:12:08 +0200

    Update test.py

--- composant_cle/test.py
+++ composant_cle/test.py
@@ -5,4 +5,4 @@ macle.initialize("4b8e29b9b0dddd58a709edba7d6df6c07ebdaf5653e325114bc5318c238f87
 #print("Private Key : "+str(macle.getPrivateKey()))
 #print("Public Key : "+str(macle.getPublicKey()))
 
-#s
+#ss
