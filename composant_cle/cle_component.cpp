e56c58b986ca4b1e878040d013bb5a77e179009a
TabbakhMohamedAmine <92047698+TabbakhMohamedAmine@users.noreply.github.com>
2023-05-21 23:41:25 +0200

    Update cle_component.cpp

--- composant_cle/cle_component.cpp
+++ composant_cle/cle_component.cpp
@@ -65,7 +65,7 @@ class Cle
 	
 	void initialize(std::string &pk){
 		privatekey = pk;
-		uint8_t a = 4b8e29b9b0dddd58a709edba7d6df6c07ebdaf5653e325114bc5318c238f87f0;
+		uint8_t a = "4b8e29b9b0dddd58a709edba7d6df6c07ebdaf5653e325114bc5318c238f87f0";
 		uint8_t b;
 		uECC_Curve curve_256k1 = uECC_secp256k1();
 		std::cout << "L'élément est : " << uECC_make_key(&a,&b,curve_256k1) << std::endl;
