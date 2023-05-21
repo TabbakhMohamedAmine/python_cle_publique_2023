93b59c46de76a0f149aae2cc2da553c3e733f4d5
TabbakhMohamedAmine <92047698+TabbakhMohamedAmine@users.noreply.github.com>
2023-05-21 18:57:14 +0200

    Update cle_component.cpp

--- composant_cle/cle_component.cpp
+++ composant_cle/cle_component.cpp
@@ -33,7 +33,7 @@ PYBIND11_MODULE(cle_component,greetings)
   
    // bindings to Cle class
     py::class_<Cle>(greetings, "Cle", py::dynamic_attr())
-        .def(py::init<const std::string &>())
+        .def(py::init<>())
         .def("getPrivateKey", &Cle::getPrivateKey)
         .def("getPublicKey", &Cle::getPublicKey);
 }
