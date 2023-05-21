dae2f5274ac149b8a1ba9df8f0cae938b196ac71
TabbakhMohamedAmine <92047698+TabbakhMohamedAmine@users.noreply.github.com>
2023-05-21 20:51:02 +0200

    Update cle_component.cpp

--- composant_cle/cle_component.cpp
+++ composant_cle/cle_component.cpp
@@ -27,6 +27,7 @@ class Cle
 		privatekey = pk;
 		uint8_t a = castStringToUint8(privatekey);
 		uint8_t b = castStringToUint8(publickey);
+		std::cout<<a;
 		//uECC_Curve curve_256k1 = uECC_secp256k1();
 		//uECC_make_key(&a,&b,curve_256k1);
 	}
