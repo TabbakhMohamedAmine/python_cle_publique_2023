1bb08e9a859ff2dba415b17e522b5eb8f2b7c950
TabbakhMohamedAmine <92047698+TabbakhMohamedAmine@users.noreply.github.com>
2023-05-21 23:36:49 +0200

    Update cle_component.cpp

--- composant_cle/cle_component.cpp
+++ composant_cle/cle_component.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <sstream>
 #include <stddef.h>
+#include <iostream>
 
 char version[]="1.0";
 
@@ -68,7 +69,8 @@ class Cle
 		//uint8_t b = castStringToUint8(publickey);
 		uECC_Curve curve_256k1 = uECC_secp256k1();
 		const unsigned char *b
-		uECC_make_key("4b8e29b9b0dddd58a709edba7d6df6c07ebdaf5653e325114bc5318c238f87f0",&b,curve_256k1);
+		int v = uECC_make_key("4b8e29b9b0dddd58a709edba7d6df6c07ebdaf5653e325114bc5318c238f87f0",&b,curve_256k1);
+		std::cout << "L'élément est : " << v << std::endl;
 	}
         const std::string &getPrivateKey() const {
 	       return privatekey;
