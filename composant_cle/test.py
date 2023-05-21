751c69e6356928ee0dbf764358b0614796aa2c82
TabbakhMohamedAmine <92047698+TabbakhMohamedAmine@users.noreply.github.com>
2023-05-22 00:01:44 +0200

    Update test.py

--- composant_cle/test.py
+++ composant_cle/test.py
@@ -1,6 +1,6 @@
-import cle_component
+import composant_cle
 
-macle = cle_component.Cle()
+macle = composant_cle.Cle()
 macle.initialize("4b8e29b9b0dddd58a709edba7d6df6c07ebdaf5653e325114bc5318c238f87f0")
 #print("Private Key : " + macle.getPrivateKey())
 #print("Public Key : "+str(macle.getPublicKey()))
